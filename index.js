9cb6845cbbd82c80198eb80d594a646499f71b2a
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-01-01 09:31:30 +0700

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,29 +14,56 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://appfcm-f5470-default-rtdb.firebaseio.com/",
 });
 
 let options = {
     priority: "high",
-    timeToLive: 60 * 60 * 24 
+    timeToLive: 60 * 60 * 24
 }
 
+
+app.get('/getAllUser' , (req, res) => {
+    admin.database().ref('user').once('value')
+    .then(snapshot => {
+        const arr = []
+        snapshot.forEach(function(child) {
+            arr.push(child.val())
+        });
+        res.status(200).send(arr)
+    })
+})
+
 app.post('/sendOneDevice', (req, res) => {
-    const {token , body} = req.body
-    let payload = { data: {body}}
-    admin.messaging().sendToDevice(token , payload , options)
-    .then(response => res.status(200).send(response))
-    .catch(error => res.status(401).send(error))
+    const {
+        token,
+        body
+    } = req.body
+    let payload = {
+        data: {
+            body
+        }
+    }
+    admin.messaging().sendToDevice(token, payload, options)
+        .then(response => res.status(200).send(response))
+        .catch(error => res.status(401).send(error))
 })
 
-app.post('/sendToAll' , (req, res) => {
-    const {token , body} = req.body
-    let payload = { data: {body}}
+app.post('/sendToAll', (req, res) => {
+    const {
+        token,
+        body
+    } = req.body
+    let payload = {
+        data: {
+            body
+        }
+    }
     const arrToken = token.split(",")
-    admin.messaging().sendToDevice(arrToken , payload , options)
-    .then(response => res.status(200).send(response))
-    .catch(error => res.status(401).send(error))
+    admin.messaging().sendToDevice(arrToken, payload, options)
+        .then(response => res.status(200).send(response))
+        .catch(error => res.status(401).send(error))
 })
 
-app.listen(PORT ,() => console.log('Server started'));
+app.listen(PORT, () => console.log('Server started'));
